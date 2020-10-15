2f916ca2260935c6ae7517a11dfca13ba69190d0
José Martins <j.msantos.netto@gmail.com>
2020-10-15 19:59:40 -0300

    Initial Commit

---
+++ BrowserApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrowserApp
+//
+//  Created by Jose Martins on 15/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
